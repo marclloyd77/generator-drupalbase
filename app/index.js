c495e20c1681ac91d5a21ecd3b0490016ae4ba65
Marc Lloyd <marc@marclloyd.co.uk>
2014-02-06 21:36:09 +0000

    Drupal download Create the configuration file and grant permissions Create database

--- app/index.js
+++ app/index.js
@@ -1,50 +1,94 @@
 'use strict';
-var util = require('util');
-var path = require('path');
-var yeoman = require('yeoman-generator');
+
+//1) Download Drupal
+
+
+var util = require('util'),
+    path = require('path'),
+    yeoman = require('yeoman-generator'),
+    shell = require('shelljs');
 
 
 var DrupalbaseGenerator = module.exports = function DrupalbaseGenerator(args, options, config) {
-  yeoman.generators.Base.apply(this, arguments);
+    yeoman.generators.Base.apply(this, arguments);
 
-  this.on('end', function () {
-    this.installDependencies({ skipInstall: options['skip-install'] });
-  });
+    this.on('end', function () {
+        this.installDependencies({ skipInstall: options['skip-install'] });
+    });
 
-  this.pkg = JSON.parse(this.readFileAsString(path.join(__dirname, '../package.json')));
+    this.pkg = JSON.parse(this.readFileAsString(path.join(__dirname, '../package.json')));
 };
 
 util.inherits(DrupalbaseGenerator, yeoman.generators.Base);
 
 DrupalbaseGenerator.prototype.askFor = function askFor() {
-  var cb = this.async();
+    var cb = this.async();
 
-  // have Yeoman greet the user.
-  console.log(this.yeoman);
+    // have Yeoman greet the user.
+    console.log(this.yeoman);
 
-  var prompts = [{
-    type: 'confirm',
-    name: 'someOption',
-    message: 'Would you like to enable this option?',
-    default: true
-  }];
+    var prompts = [
+        {
+            type: 'input',
+            name: 'siteURL',
+            message: 'The Site URL (e.g. 127.0.0.1/drupal or mylocalsite.co.uk)'
+        },
+        {
+            type: 'input',
+            name: 'drupalVersion',
+            message: 'Enter Drupal version to download',
+            default: '7.26'
+        },
+        {
+            type: 'input',
+            name: 'dbName',
+            message: 'Database Name'
+        },
+        {
+            type: 'input',
+            name: 'dbUser',
+            message: 'Database Username',
+            default: 'root'
+        },
+        {
+            type: 'input',
+            name: 'dbPass',
+            message: 'Database Password',
+            default: 'root'
+        }
+    ];
 
-  this.prompt(prompts, function (props) {
-    this.someOption = props.someOption;
+    this.prompt(prompts, function (props) {
+        this.siteURL = props.siteURL;
+        this.drupalVersion = props.drupalVersion;
+        this.dbName = props.dbName;
+        this.dbUser = props.dbUser;
+        this.dbPass = props.dbPass;
 
-    cb();
-  }.bind(this));
+        cb();
+    }.bind(this));
 };
 
-DrupalbaseGenerator.prototype.app = function app() {
-  this.mkdir('app');
-  this.mkdir('app/templates');
+DrupalbaseGenerator.prototype.DownloadDrupal = function DownloadDrupal() {
+    var cb   = this.async();
 
-  this.copy('_package.json', 'package.json');
-  this.copy('_bower.json', 'bower.json');
+    this.log.writeln('\n***********************************************\n** Downloading the latest Version of Drupal **\n************************************************');
+    this.tarball('http://ftp.drupal.org/files/projects/drupal-' + this.drupalVersion + '.zip', './', cb);
 };
 
-DrupalbaseGenerator.prototype.projectfiles = function projectfiles() {
-  this.copy('editorconfig', '.editorconfig');
-  this.copy('jshintrc', '.jshintrc');
+
+// Create the configuration file and grant permissions
+DrupalbaseGenerator.prototype.configAndPermissions = function configAndPermissions() {
+
+    this.log.writeln('\n***********************************************\n** Creating configuration file and granting permissions **\n************************************************');
+    shell.exec('cp sites/default/default.settings.php sites/default/settings.php')
+    shell.exec('chmod a+w sites/default/settings.php')
+    shell.exec('chmod a+w sites/default')
+};
+
+//Create database
+DrupalbaseGenerator.prototype.CreateDatabase = function CreateDatabase() {
+
+    this.log.writeln('\n***********************\n** Creating database **\n***********************');
+    shell.exec('mysql --user="' + this.dbUser + '" --password="' + this.dbPass + '" -e "create database ' + this.dbName + '"');
 };
